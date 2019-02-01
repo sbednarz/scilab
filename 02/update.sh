a88c5165bd34f8f1db3ea311038d8de4b547b582
Szczepan Bednarz <sbednarz@pk.edu.pl>
2019-02-02 00:50:03 +0100

    update mass balance examples

---
+++ 02/update.sh
@@ -0,0 +1,8 @@
+cat mass_balance_01.md > mass_balance_01_ex.md
+echo '```scilab' >> mass_balance_01_ex.md
+cat mass_balance_01.sce >> mass_balance_01_ex.md
+echo '```' >> mass_balance_01_ex.md
+
+
+
+cat mass_balance_01_ex.md > README.md
